9a34cceeafdf9201d0abd6671d3268c9f0473b71
Thibaut Jacob <thibautquentinjacob@gmail.com>
2020-07-10 08:49:16 +0200

    feat: :sparkles: Wrap up Neuron class implementation

--- spec/models/neuron.model.spec.ts
+++ spec/models/neuron.model.spec.ts
@@ -1,79 +1,184 @@
 import {
   ActivationFunction,
+  MemoizedNeuronInvalidArgumentError,
   Neuron,
   NeuronInput,
   Vector,
 } from '../../src/models';
 
-describe('Neuron model', () => {
-  let neuron: Neuron;
-  const weights: Vector = new Vector({ values: [0, 1] });
-  const bias = 4;
+describe('Neuron', () => {
+  describe('model', () => {
+    let neuron: Neuron;
+    const weights: Vector = new Vector({ values: [0, 1] });
+    const bias = 4;
 
-  beforeEach(() => {
-    neuron = new Neuron({
-      weights: weights,
-      bias: bias,
+    beforeEach(() => {
+      neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+      });
+    });
+
+    it('should be defined', () => {
+      expect(neuron).toBeDefined();
+    });
+
+    it('should be sealed', () => {
+      expect(Object.isSealed(neuron)).toBeTruthy();
+    });
+
+    it('weights field should have correct value - defined', () => {
+      expect(neuron.weights).toEqual(weights);
+    });
+
+    it('weights field should have correct value - null', () => {
+      neuron = new Neuron({
+        weights: null,
+        bias: bias,
+      });
+      expect(neuron.weights).toEqual(new Vector({ values: [] }));
+    });
+
+    it('Bias field should have correct value - defined', () => {
+      expect(neuron.bias).toEqual(bias);
+    });
+
+    it('Bias field should have correct value - null', () => {
+      neuron = new Neuron({
+        weights: weights,
+        bias: null,
+      });
+      expect(neuron.bias).toEqual(0);
+    });
+
+    it('Memoize field should have correct default value', () => {
+      expect(neuron.memoize).toEqual(true);
+    });
+
+    it('Memoize field should have correct value - false', () => {
+      neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+        memoize: false,
+      });
+      expect(neuron.memoize).toEqual(false);
+    });
+
+    it('Memoize field should have correct value - true', () => {
+      neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+        memoize: true,
+      });
+      expect(neuron.memoize).toEqual(true);
+    });
+
+    it('output field should be as expected', () => {
+      expect(neuron['_output']).toEqual({});
     });
   });
 
-  it('should be defined', () => {
-    expect(neuron).toBeDefined();
-  });
-
-  it('should be frozen', () => {
-    expect(Object.isFrozen(neuron)).toBeTruthy();
-  });
-
-  it('weights field should have correct value - defined', () => {
-    expect(neuron.weights).toEqual(weights);
-  });
-
-  it('weights field should have correct value - null', () => {
-    neuron = new Neuron({
-      weights: null,
-      bias: bias,
+  describe('Feed forward', () => {
+    let neuron: Neuron;
+    const input: NeuronInput = new NeuronInput({
+      label: 'x',
+      values: new Vector({ values: [2, 3] }),
     });
-    expect(neuron.weights).toEqual(new Vector({ values: [] }));
-  });
+    const activationFunction: ActivationFunction = (value: number) => {
+      return 1 / (1 + Math.exp(-value));
+    };
+    const weights: Vector = new Vector({ values: [0, 1] });
+    const bias = 4;
 
-  it('Bias field should have correct value - defined', () => {
-    expect(neuron.bias).toEqual(bias);
-  });
-
-  it('Bias field should have correct value - null', () => {
-    neuron = new Neuron({
-      weights: weights,
-      bias: null,
+    beforeEach(() => {
+      neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+      });
     });
-    expect(neuron.bias).toEqual(0);
-  });
-});
 
-describe('Feed forward', () => {
-  let neuron: Neuron;
-  const neuronInput: NeuronInput = new NeuronInput({
-    label: 'x',
-    values: new Vector({ values: [2, 3] }),
-  });
-  const activationFunction: ActivationFunction = (value: number) => {
-    return 1 / (1 + Math.exp(-value));
-  };
-  const weights: Vector = new Vector({ values: [0, 1] });
-  const bias = 4;
+    it('should return expected value', () => {
+      expect(
+        Math.round(
+          neuron.feedForward(input, activationFunction) * Math.pow(10, 6)
+        ) / Math.pow(10, 6)
+      ).toEqual(0.999089);
+    });
 
-  beforeEach(() => {
-    neuron = new Neuron({
-      weights: weights,
-      bias: bias,
+    it('should raise an error - missing input', () => {
+      expect(() =>
+        neuron.feedForward((null as unknown) as NeuronInput, activationFunction)
+      ).toThrow(new MemoizedNeuronInvalidArgumentError('Input missing'));
+    });
+
+    it('should raise an error - missing activation function', () => {
+      expect(() =>
+        neuron.feedForward(input, (null as unknown) as ActivationFunction)
+      ).toThrow(
+        new MemoizedNeuronInvalidArgumentError('Activation function missing')
+      );
+    });
+
+    it('should store and read result from cache', () => {
+      const neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+      });
+      const outputValue = 0.5;
+      const activationFunction: jasmine.Spy = jasmine
+        .createSpy('activationFunction')
+        .and.returnValue(outputValue);
+      neuron.feedForward(input, activationFunction);
+      expect(neuron['_output']['2-3']).toEqual(outputValue);
+      neuron.feedForward(input, activationFunction);
+
+      expect(activationFunction).toHaveBeenCalledTimes(1);
+      expect(activationFunction).toHaveBeenCalledWith(7);
+    });
+
+    it('should not read result from cache', () => {
+      const neuron = new Neuron({
+        weights: weights,
+        bias: bias,
+        memoize: false,
+      });
+      const outputValue = 0.5;
+      const activationFunction: jasmine.Spy = jasmine
+        .createSpy('activationFunction')
+        .and.returnValue(outputValue);
+      neuron.feedForward(input, activationFunction);
+      expect(neuron['_output']).toEqual({});
+      neuron.feedForward(input, activationFunction);
+
+      expect(activationFunction).toHaveBeenCalledTimes(2);
+      expect(activationFunction).toHaveBeenCalledWith(7);
     });
   });
 
-  it('should return expected value', () => {
-    expect(
-      Math.round(
-        neuron.feedForward(neuronInput, activationFunction) * Math.pow(10, 6)
-      ) / Math.pow(10, 6)
-    ).toEqual(0.999089);
+  describe('hashInput', () => {
+    let neuron: Neuron;
+
+    beforeEach(() => {
+      neuron = new Neuron({
+        weights: new Vector({ values: [1, 2] }),
+        bias: 4,
+      });
+    });
+
+    it('should return expected result - defined', () => {
+      const input: NeuronInput = new NeuronInput({
+        label: 'x',
+        values: new Vector({ values: [3, 5] }),
+      });
+      expect(neuron['_hashInput'](input)).toEqual('3-5');
+    });
+
+    it('should return expected result - negative', () => {
+      const input: NeuronInput = new NeuronInput({
+        label: 'x',
+        values: new Vector({ values: [-3, -5] }),
+      });
+      expect(neuron['_hashInput'](input)).toEqual('-3--5');
+    });
   });
 });
--- src/models/neuron.model.ts
+++ src/models/neuron.model.ts
@@ -1,24 +1,36 @@
-import { Helpers } from '../helpers';
 import { NeuronInput } from './neuron-input.model';
 import { Vector } from './vector.model';
 
 export type ActivationFunction = (value: number) => number;
 
+export class MemoizedNeuronInvalidArgumentError extends Error {
+  public constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, MemoizedNeuronInvalidArgumentError.prototype);
+  }
+}
+
 export class Neuron {
+  private _output: { [key: string]: number };
   public readonly weights: Vector;
   public readonly bias: number;
+  public readonly memoize: boolean;
 
   public constructor({
     weights,
     bias,
+    memoize,
   }: {
     weights: Vector | null;
     bias: number | null;
+    memoize?: boolean;
   }) {
     this.weights = this._processWeights(weights);
     this.bias = this._processBias(bias);
+    this.memoize = this._processMemoize(memoize);
+    this._output = {};
 
-    Helpers.deepFreeze(this);
+    Object.seal(this);
   }
 
   private _processWeights(weights: Vector | null): Vector {
@@ -29,10 +41,41 @@ export class Neuron {
     return bias ? bias : 0;
   }
 
+  private _processMemoize(memoize: boolean | undefined): boolean {
+    return !memoize && memoize !== false ? true : memoize;
+  }
+
+  /**
+   * Generate a key identifying the neuron input.
+   * @param {NeuronInput} input - Input
+   * @returns {string} Hash
+   */
+  private _hashInput(input: NeuronInput): string {
+    return input.values.values.join('-');
+  }
+
   public feedForward(
     input: NeuronInput,
     activationFunction: ActivationFunction
   ): number {
-    return activationFunction(this.weights.dot(input.values) + this.bias);
+    if (!input) {
+      throw new MemoizedNeuronInvalidArgumentError('Input missing');
+    }
+    if (!activationFunction) {
+      throw new MemoizedNeuronInvalidArgumentError(
+        'Activation function missing'
+      );
+    }
+    const hash: string = this._hashInput(input);
+    // If the result has not already been computed
+    if (!this._output[hash]) {
+      const result: number = activationFunction(
+        this.weights.dot(input.values) + this.bias
+      );
+      if (this.memoize) {
+        this._output[hash] = result;
+      }
+    }
+    return this._output[hash];
   }
 }
