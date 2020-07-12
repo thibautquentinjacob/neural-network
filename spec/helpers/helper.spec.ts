50802edc6d2c07242f89aa670d7f8413cd78e684
Thibaut Jacob <thibautquentinjacob@gmail.com>
2020-07-12 12:36:22 +0200

    feat: :sparkles: Add methods to shift data

--- spec/helpers/helper.spec.ts
+++ spec/helpers/helper.spec.ts
@@ -1,4 +1,4 @@
-import { Helpers } from '../../src/helpers';
+import { DataEntry, Helpers } from '../../src/helpers';
 
 describe('Deepfreeze', () => {
   let object: { [key: string]: number | string | boolean | object };
@@ -39,5 +39,126 @@ describe('Deepfreeze', () => {
     } catch (e) {
       expect(object.boolean).toEqual(true);
     }
+  });
+});
+
+describe('shiftDataset', () => {
+  let dataset: DataEntry[];
+  const columns: string[] = ['length', 'width'];
+  const fun = (
+    previous: number,
+    current: number,
+    index: number,
+    array: number[]
+  ): number => {
+    const length: number = array.length;
+    if (index === length - 1) {
+      return (previous + current) / length;
+    }
+    return previous + current;
+  };
+  const initialValue = 0;
+  beforeEach(() => {
+    dataset = [
+      {
+        id: 1,
+        length: 152,
+        width: 40,
+        depth: 10,
+      },
+      {
+        id: 2,
+        length: 13,
+        width: 70,
+        depth: 80,
+      },
+      {
+        id: 3,
+        length: 132,
+        width: 100,
+        depth: 100,
+      },
+      {
+        id: 4,
+        length: 200,
+        width: 120,
+        depth: 5,
+      },
+    ];
+  });
+
+  it('should shift dataset as expected', () => {
+    const expectedDataset: DataEntry[] = [
+      {
+        id: 1,
+        length: 27.75,
+        width: -42.5,
+        depth: 10,
+      },
+      {
+        id: 2,
+        length: -111.25,
+        width: -12.5,
+        depth: 80,
+      },
+      {
+        id: 3,
+        length: 7.75,
+        width: 17.5,
+        depth: 100,
+      },
+      {
+        id: 4,
+        length: 75.75,
+        width: 37.5,
+        depth: 5,
+      },
+    ];
+    expect(Helpers.shiftDataset(dataset, columns, fun, initialValue)).toEqual(
+      expectedDataset
+    );
+  });
+
+  describe('_shiftEntry', () => {
+    it('should shift data entry as expected', () => {
+      const expectedEntry: DataEntry = {
+        id: 1,
+        length: 27.75,
+        width: -42.5,
+        depth: 10,
+      };
+      const shifts: { [key: string]: number } = {
+        length: 124.25,
+        width: 82.5,
+      };
+      expect(Helpers['_shiftEntry'](dataset[0], shifts)).toEqual(expectedEntry);
+    });
+
+    it('should not shift what is not a number', () => {
+      const expectedEntry: DataEntry = {
+        id: 1,
+        length: 27.75,
+        width: '40',
+        depth: 10,
+      };
+      const shifts: { [key: string]: number } = {
+        length: 124.25,
+        width: 82.5,
+      };
+      dataset[0].width = '40';
+      expect(Helpers['_shiftEntry'](dataset[0], shifts)).toEqual(expectedEntry);
+    });
+  });
+
+  describe('_computeShift', () => {
+    it('should compute expected shift', () => {
+      const expectedShifts: { [key: string]: number } = {
+        length: 124.25,
+        width: 82.5,
+      };
+      expect(
+        Helpers['_computeShifts'](dataset, columns, fun, initialValue)
+      ).toEqual(expectedShifts);
+    });
   });
 });
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,5 @@
+export type DataEntry = { [key: string]: string | number | boolean };
+
 export class Helpers {
   /**
    * Prevent the fields of an object to be mutated by freezing all its
@@ -18,5 +20,84 @@ export class Helpers {
     });
 
     return Object.freeze(object);
+  }
+
+  /**
+   * Compute shifts of numeric columns of a given dataset, using a provided
+   * function and initial value.
+   *
+   * @param {DataEntry[]} data - Dataset to shift
+   * @param {string[]} columns - Columns to be shifted
+   * @param {(previous: number, current: number) => number} fun - Function to
+   * use to compute the shift
+   * @param {number} initialValue - Initial shift value
+   * @returns {{ [key: string]: number }} Record containing the shift for each
+   * column
+   */
+  private static _computeShifts(
+    data: DataEntry[],
+    columns: string[],
+    fun: (
+      previous: number,
+      current: number,
+      index: number,
+      array: number[]
+    ) => number,
+    initialValue: number
+  ): { [key: string]: number } {
+    const shifts: { [key: string]: number } = {};
+
+    columns.forEach((column: string) => {
+      const values: number[] = data.map(
+        (entry: DataEntry) => entry[column] as number
+      );
+      shifts[column] = values.reduce(fun, initialValue) as number;
+    });
+
+    return shifts;
+  }
+
+  /**
+   * Shift data entry using provided shift record
+   *
+   * @param {DataEntry} entry - Data entry
+   * @param {{ [key: string]: number }} shifts - Shifts to apply
+   * @returns {DataEntry} Shifted data entry
+   */
+  private static _shiftEntry(
+    entry: DataEntry,
+    shifts: { [key: string]: number }
+  ): DataEntry {
+    Object.keys(shifts).forEach((column: string) => {
+      if (column in entry && typeof entry[column] === 'number') {
+        entry[column] = (entry[column] as number) - shifts[column];
+      }
+    });
+    return entry;
+  }
+
+  public static shiftDataset(
+    data: DataEntry[],
+    columns: string[],
+    fun: (
+      previous: number,
+      current: number,
+      index: number,
+      array: number[]
+    ) => number,
+    initialValue: number
+  ): DataEntry[] {
+    // Computes data shift for numerical columns using the provided function
+    const shifts: { [key: string]: number } = Helpers._computeShifts(
+      data,
+      columns,
+      fun,
+      initialValue
+    );
+
+    // Apply data shift to each entry
+    return data.map((entry: DataEntry) => {
+      return Helpers._shiftEntry(entry, shifts);
+    });
   }
 }
